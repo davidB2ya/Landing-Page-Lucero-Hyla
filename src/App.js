ffda31eb449f0fb16574045f3aa67173a8eb2ae8
davidB2ya <dbedoya0408@gmail.com>
2021-09-02 11:04:34 -0500

    Import detail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
-import BannerLarge from './components/BannerLarge/BannerLarge';
+// import BannerLarge from './components/BannerLarge/BannerLarge';
 import Certificates from './components/Certificates-Hyla/Certificates';
 import Models from './components/Equipment Models/Models';
 import Header from './components/Header/Header';
